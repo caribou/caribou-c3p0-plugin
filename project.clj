10242235c97445eb7fd3e7e7cd9a7b07fbea7152
Justin Smith <noisesmith@gmail.com>
2013-12-07 07:14:10 -0800

    bumping version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject caribou/c3p0-plugin "0.0.1"
+(defproject caribou/c3p0-plugin "0.0.2"
   :description "A plugin to turn your db config into a connection pool."
-  :dependencies [[caribou/caribou-plugin "0.0.2"]
+  :dependencies [[caribou/caribou-plugin "0.0.3"]
                  [org.tobereplaced/jdbc-pool "0.1.0"]]
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
